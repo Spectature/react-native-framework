bf29488c498eca17686dd011053ab5e563c666e9
杜天宇 <17862705909@163.com>
2025-05-05 12:06:08 +0800

    feat: 修改react-native-dotenv加载方式

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    'module:react-native-dotenv',
-    {
-      moduleName: '@env',
-      path: `.env.${process.env.NODE_ENV}`,
-      blacklist: null,
-      whitelist: null,
-      safe: false,
-      allowUndefined: true,
-    },
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: `.env.${process.env.NODE_ENV}`,
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
     'react-native-reanimated/plugin', // 注意：要放在最后一项
   ],
 };
